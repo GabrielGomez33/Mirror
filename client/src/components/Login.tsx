a02b9f4dd9a9087a043900af6687c3cca886841f
GabrielGomez33 <gabrielelythgomez@gmail.com>
2026-06-10 09:09:11 -0400

    More aesthetic upgrades to login and register steps 8

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -846,7 +846,7 @@ const LogUserIn: React.FC = () => {
             type="submit"
             disabled={isLoading || lockoutInfo.locked || !formReady}
             className={`${submitButtonClass} w-full`}
-            style={{ borderRadius: '1rem' }}
+            style={{ borderRadius: '1rem', minWidth:'90vw' }}
             aria-busy={isLoading}
           >
             {isLoading ? (
